98fbb0014085c8459144e355a709b09e6907639a
sangeetha-sv24 <155470096+sangeetha-sv24@users.noreply.github.com>
2024-03-28 11:50:30 +0400

    Update PrintVowels.java
    Printing Vowels count using rejex

--- src/main/java/sangee/PrintVowels.java
+++ src/main/java/sangee/PrintVowels.java
@@ -5,17 +5,9 @@ public class PrintVowels {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 String s="sangeetha";
-         for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == 'a')
-                System.out.println(s.charAt(i));
-            else if(s.charAt(i) == 'e')
-                System.out.println(s.charAt(i));            
-            else if(s.charAt(i) == 'i')
-                System.out.println(s.charAt(i));
-            else if(s.charAt(i) == 'o')
-                System.out.println(s.charAt(i));
-            else if(s.charAt(i)=='u')
-                System.out.println(s.charAt(i));
+         int coubt=s.replaceAll("[^aeiouAEIOU]","").length();
+		System.out.println(count);
+		System.out.println(s);
         }
 }
 	
