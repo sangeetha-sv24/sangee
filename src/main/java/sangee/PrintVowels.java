e5abd1993058a806e666b9fe0e4991c9af08b8fe
sangeetha-sv24 <155470096+sangeetha-sv24@users.noreply.github.com>
2024-03-28 11:53:03 +0400

    Update PrintVowels.java
    Print Vowels using rejex

--- src/main/java/sangee/PrintVowels.java
+++ src/main/java/sangee/PrintVowels.java
@@ -5,9 +5,13 @@ public class PrintVowels {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 String s="sangeetha";
-         int coubt=s.replaceAll("[^aeiouAEIOU]","").length();
+         int count=s.replaceAll("[^aeiouAEIOU]","").length();
 		System.out.println(count);
 		System.out.println(s);
+		String vowels=s.replaceAll("[^aeiouAEIOU]","");
+		System.out.println(vowels);
+
+	}
         }
 }
 	
